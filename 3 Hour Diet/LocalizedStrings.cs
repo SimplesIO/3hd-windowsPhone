46a4b5d772a574e90c35487cddd4e93fe462810c
Rafael Assis <rafazassis@gmail.com>
2014-05-18 21:13:55 -0300

    First Commit Windows Phone !!

---
+++ 3 Hour Diet/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using _3_Hour_Diet.Resources;
+
+namespace _3_Hour_Diet
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
